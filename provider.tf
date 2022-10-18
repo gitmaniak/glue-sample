33fe3adb736966e451d21aab8d58b60d7334ff1b
gitmaniak <51684783+gitmaniak@users.noreply.github.com>
2022-10-18 09:15:31 -0400

    Create provider.tf

---
+++ provider.tf
@@ -0,0 +1,23 @@
+terraform {
+  required_version        = ">= 1.1.0"
+  required_providers {
+    aws                   = ">= 4.29.0"
+  }
+}
+
+provider "aws" {
+  profile                 = var.aws_profile
+  region                  = var.aws_region
+}
+
+locals {
+  aws_region = var.aws_region
+}
+
+terraform {
+  backend "s3" {
+    bucket = "543552320750-tfstate-backend-bucket"
+    key    = "network/tfstate.tf"
+    region = local.aws_region
+  }
+}
